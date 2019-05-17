b0fd50966578883a2310006c9e0ae2b310b59678
CamusQ <1429143585@qq.com>
2019-05-17 16:21:47 +0800

    TankWar0.2

--- src/com/tankwar/client/TankClient.java
+++ src/com/tankwar/client/TankClient.java
@@ -1,6 +1,8 @@
 package com.tankwar.client;
 
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 /**
  * @auther camus
@@ -9,9 +11,15 @@ import java.awt.*;
 public class TankClient extends Frame {
 
     public void lauchFrame(){
-        this.setLocation(400, 300);
+        this.setLocation(400, 100);
         this.setSize(800, 600);
-
+        this.setTitle("TankWar");
+        this.addWindowListener(new WindowAdapter() {//匿名类适合用在类短小、不涉及拓展、不涉及重要的业务逻辑
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);//表示正常退出
+            }
+        });
+        this.setResizable(false);
         this.setVisible(true);
     }
 
