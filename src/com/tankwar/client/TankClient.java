d30053a6adec60822719372b2560416b9e624c33
CamusQ <1429143585@qq.com>
2019-05-17 19:26:27 +0800

    TankWar0.6

--- src/com/tankwar/client/TankClient.java
+++ src/com/tankwar/client/TankClient.java
@@ -1,6 +1,8 @@
 package com.tankwar.client;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -13,20 +15,40 @@ public class TankClient extends Frame {
     int x = 50;
     int y = 50;
 
+    public static final int GAME_WIDTH = 800;
+    public static final int GAME_HEIGHT = 600;
+
+
+    Image offScreenImage = null;
+
     public void paint(Graphics g) {
 
         Color c = g.getColor();
         g.setColor(Color.red);
         g.fillOval(x, y, 30, 30);//画一个实心圆
         g.setColor(c);
+    }
 
-        y += 1;
+    public void update(Graphics g) {
+
+        if(offScreenImage == null){
+            offScreenImage = this.createImage(GAME_WIDTH, GAME_HEIGHT);
+        }
+
+        Color c = g.getColor();
+        Graphics gOffScreen = offScreenImage.getGraphics();
+        gOffScreen.setColor(Color.green);
+        gOffScreen.fillRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+        gOffScreen.setColor(c);
+        paint(gOffScreen);
+        g.drawImage(offScreenImage, 0, 0, null);
+
 
     }
 
     public void lauchFrame(){
         this.setLocation(400, 100);
-        this.setSize(800, 600);
+        this.setSize(GAME_WIDTH, GAME_HEIGHT);
         this.setTitle("TankWar");
         this.addWindowListener(new WindowAdapter() {//匿名类适合用在类短小、不涉及拓展、不涉及重要的业务逻辑
             public void windowClosing(WindowEvent e) {
@@ -36,6 +58,9 @@ public class TankClient extends Frame {
         });
         this.setResizable(false);
         this.setBackground(Color.green);
+
+        this.addKeyListener(new keyMonitor());
+
         this.setVisible(true);
 
         //什么时候启动重画线程（窗口打开以后）
@@ -53,13 +78,28 @@ public class TankClient extends Frame {
         @Override
         public void run() {
             while(true){
-                repaint();//repaint 内部自动调用paint
+                repaint();//repaint 内部自动调用paint（先调用update 再调用paint）
                 try {
                     Thread.sleep(20);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+        }
+    }
+
+    private class keyMonitor extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key){
+                case KeyEvent.VK_RIGHT:x += 1;break;
+                case KeyEvent.VK_LEFT:x -= 1;break;
+                case KeyEvent.VK_UP:y -= 1;break;
+                case KeyEvent.VK_DOWN:y += 1;break;
+                default:break;
+            }
+
         }
     }
 
