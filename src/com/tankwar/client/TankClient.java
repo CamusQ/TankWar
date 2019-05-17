cb6e455dcf9086a63ad03bd890b5ebe9ffbd8be6
CamusQ <1429143585@qq.com>
2019-05-17 17:25:52 +0800

    TankWar0.4

--- src/com/tankwar/client/TankClient.java
+++ src/com/tankwar/client/TankClient.java
@@ -10,6 +10,20 @@ import java.awt.event.WindowEvent;
  */
 public class TankClient extends Frame {
 
+    int x = 50;
+    int y = 50;
+
+    public void paint(Graphics g) {
+
+        Color c = g.getColor();
+        g.setColor(Color.red);
+        g.fillOval(x, y, 30, 30);//画一个实心圆
+        g.setColor(c);
+
+        y += 1;
+
+    }
+
     public void lauchFrame(){
         this.setLocation(400, 100);
         this.setSize(800, 600);
@@ -18,14 +32,35 @@ public class TankClient extends Frame {
             public void windowClosing(WindowEvent e) {
                 System.exit(0);//表示正常退出
             }
+
         });
         this.setResizable(false);
+        this.setBackground(Color.green);
         this.setVisible(true);
+
+        //什么时候启动重画线程（窗口打开以后）
+        new Thread(new paintThread()).start();
     }
 
     public static void main(String[] args) {
         TankClient tc = new TankClient();
         tc.lauchFrame();
+    }
+
+    //内部类。可以非常方便的访问包装类的方法。不方便公开的，只为包装类服务类应当定义为内部类
+    private class paintThread implements Runnable{
+
+        @Override
+        public void run() {
+            while(true){
+                repaint();//repaint 内部自动调用paint
+                try {
+                    Thread.sleep(20);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
